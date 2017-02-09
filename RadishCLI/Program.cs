89da061d82fcdeae6fe0b97886fddec148577cf3
kraptop@laptop <benoit.delayen@free.fr>
2017-02-09 02:07:05 +0100

    Create lib and cli solution

---
+++ RadishCLI/Program.cs
@@ -0,0 +1,17 @@
+﻿using RadishCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadishCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using RadishCore version " + CoreLibrary.Version);
+        }
+    }
+}
---
+++ RadishCore/CoreLibrary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadishCore
+{
+    public class CoreLibrary
+    {
+        public const int Version = 1;
+    }
+}
